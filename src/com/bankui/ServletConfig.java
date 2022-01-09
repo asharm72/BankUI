b32a2a6b1df53ec6fafa6120d8638b5798b689da
Sharma <ankita.o.sharma@capgemini.com>
2022-01-09 18:42:33 +0530

    mvc configuration

---
+++ src/com/bankui/ServletConfig.java
@@ -0,0 +1,32 @@
+package com.bankui;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.bankui.controller"})
+public class ServletConfig implements WebMvcConfigurer{
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("WEB-INF/jsps/"); //WEB-INF/jsps/index.jsp
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	 
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource= 
+				new DriverManagerDataSource("jdbc:postgresql://localhost:5432/postgres", "postgres", "12345");
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		return dataSource; 
+	}
+}
